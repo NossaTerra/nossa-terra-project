3253b16494f6821892190ef5fb555232d69ec4af
Felipe Emos <felipe.emos.computacao@gmail.com>
2023-12-06 12:24:40 -0300

    style: format project with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#025d6d] to-[#2c2815] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">Create T3 App</h1>
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Create T3 App
+        </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
