e04d267f47a44a121b176f3258f15bc62008a437
Felipe Emos <felipe.emos.computacao@gmail.com>
2023-12-28 01:45:47 -0300

    feat(LoginRegisterFlow): add screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,101 +1,67 @@
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
-import { type ClassNameProps, cn } from "src/utils/ui";
-import { NossaTerraLogo } from "~/components/common/NossaTerraLogo";
-import ImageCarousel from "~/components/common/ImageCarrousel";
+import { ChooseRoleScreen } from "~/screens/LoginRegisterFlow/screens/ChooseRoleScreen";
+import { FirstDataStepScreen } from "~/screens/LoginRegisterFlow/screens/FirstDataStepScreen";
+import { GreetingScreen } from "~/screens/LoginRegisterFlow/screens/GreetingScreen";
+import { WelcomeBackScreen } from "~/screens/LoginRegisterFlow/screens/WelcomeBackScreen";
+import { useLoginRegisterFlow } from "~/screens/LoginRegisterFlow/state/machine";
+import {
+  AnimatePresence,
+  type Variants,
+  motion,
+  type Transition,
+} from "framer-motion";
 
-function MainContent({ className }: ClassNameProps) {
-  return (
-    <main
-      className={cn(
-        "flex items-center justify-center md:justify-start",
-        className,
-      )}
-    >
-      <div
-        className={cn(
-          "w-full max-w-[28rem] md:max-w-none",
-          "flex flex-col md:flex-row",
-
-          // Align
-          "items-center md:items-start",
-          "justify-center",
-
-          // Spacing
-          "px-8 py-10",
-          "gap-10 md:gap-16 lg:gap-28",
-        )}
-      >
-        <h1
-          className={cn(
-            "font-poppins-700 text-headingPrimary",
-            "text-left md:text-right",
-            "text-2xl md:text-3xl lg:text-4xl",
-          )}
-        >
-          Seja bem vindo(a) à{" "}
-          <span
-            className={cn(
-              "font-poppins-800 text-headingSecondary",
-              "text-3xl md:text-4xl lg:text-5xl",
-              "inline-block md:block",
-            )}
-          >
-            Nossa Terra
-          </span>
-        </h1>
-
-        <form className="w-full md:max-w-xs lg:max-w-sm">
-          <div className="mb-4 w-full">
-            <label
-              className="block text-sm font-medium text-gray-700"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <Input
-              className="mt-3x w-full md:mt-0"
-              id="email"
-              placeholder="Email"
-            />
-          </div>
-          <Button className="w-full bg-backgroundTertiary text-basedDark hover:bg-green-400">
-            Continuar
-          </Button>
-        </form>
-      </div>
-    </main>
-  );
+enum Direction {
+  Left = "left",
+  Right = "right",
 }
 
+const variants = {
+  enter: (direction: Direction) => ({
+    x: direction === Direction.Left ? "-100%" : "100%",
+    opacity: 0,
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    zIndex: 1,
+  },
+  exit: (direction: Direction) => ({
+    x: direction === Direction.Left ? "100%" : "-100%",
+    opacity: 0,
+    zIndex: 0,
+  }),
+} as const satisfies Variants;
+
+const transition: Transition = {
+  x: { type: "spring", stiffness: 300, damping: 30 },
+  opacity: { duration: 0.2 },
+};
+
 export default function RootScreen() {
+  const stepKey = useLoginRegisterFlow((s) => s.state.stepKey);
+  const lastCommand = useLoginRegisterFlow((s) => s.lastCommand);
+
+  const direction = lastCommand === "goBack" ? Direction.Left : Direction.Right;
+
   return (
-    <div className="flex min-h-screen flex-grow flex-col">
-      <header className="flex items-start justify-center pt-12 md:justify-end">
-        <div className="hidden px-12 md:block">
-          <NossaTerraLogo />
-        </div>
-        <ImageCarousel
-          pathArray={["", "", "", "", "", "", ""]}
-          height={220}
-          width={220}
-          className="block md:hidden"
-        />
-      </header>
-
-      <MainContent className="flex grow" />
-
-      <footer className="flex flex-col justify-center">
-        <ImageCarousel
-          pathArray={["", "", "", "", "", "", ""]}
-          height={220}
-          width={220}
-          className="hidden md:block"
-        />
-        <div className="flex justify-center p-12 md:hidden">
-          <NossaTerraLogo />
-        </div>
-      </footer>
+    <div className="w-screen overflow-x-hidden">
+      <AnimatePresence initial={false} custom={direction} mode="popLayout">
+        <motion.div
+          key={stepKey}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          custom={direction}
+          variants={variants}
+          transition={transition}
+          className="w-screen"
+        >
+          {stepKey === "greeting" && <GreetingScreen />}
+          {stepKey === "welcomeBack" && <WelcomeBackScreen />}
+          {stepKey === "chooseRole" && <ChooseRoleScreen />}
+          {stepKey === "firstDataStep" && <FirstDataStepScreen />}
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ src/screens/LoginRegisterFlow/screens/WelcomeBackScreen.tsx
@@ -0,0 +1,161 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { type ClassNameProps, cn } from "~/utils/ui";
+import { NossaTerraLogo } from "~/components/common/NossaTerraLogo";
+import ImageCarousel from "~/components/common/ImageCarrousel";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { useCallback } from "react";
+import {
+  type WelcomeBackFields,
+  useWelcomeBackSchema,
+} from "../hooks/useWelcomeBackSchema";
+import { useLoginRegisterFlow } from "../state/machine";
+import { ArrowLeftIcon } from "lucide-react";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+
+function WelcomeBackContent({ className }: ClassNameProps) {
+  const { state, resetState } = useLoginRegisterFlow();
+
+  const schema = useWelcomeBackSchema();
+  const form = useForm<WelcomeBackFields>({
+    resolver: zodResolver(schema),
+  });
+
+  const login = api.auth.login.useMutation();
+  const router = useRouter();
+  const onSubmit: SubmitHandler<WelcomeBackFields> = useCallback(
+    async ({ password }) => {
+      if (state.stepKey !== "welcomeBack") {
+        return;
+      }
+      const { email } = state.accumulatedContext;
+      await login.mutateAsync({
+        email,
+        password,
+      });
+      await router.replace("/search");
+      resetState();
+    },
+    [login, resetState, router, state.accumulatedContext, state.stepKey],
+  );
+
+  return (
+    <main
+      className={cn(
+        "flex flex-col items-start justify-start gap-10 md:justify-start",
+        "px-8 py-10 lg:px-14",
+        className,
+      )}
+    >
+      <h1
+        className={cn(
+          "font-poppins-800 text-headingSecondary",
+          "text-4xl lg:text-5xl",
+          "inline-block md:block",
+        )}
+      >
+        Que bom que você voltou!
+      </h1>
+
+      <div className="flex flex-col gap-2 text-gray-700">
+        <label>Email</label>
+        <span>
+          {state.stepKey === "welcomeBack"
+            ? state.accumulatedContext.email
+            : "----"}
+        </span>
+      </div>
+
+      <Form {...form}>
+        <form
+          className="w-full md:max-w-xs lg:max-w-sm"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field, fieldState }) => (
+              <FormItem className="mb-4 w-full text-gray-700">
+                <FormLabel
+                  className="block text-sm font-medium"
+                  htmlFor="email"
+                >
+                  Senha
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="mt-3x w-full md:mt-0"
+                    id="email"
+                    placeholder="Senha"
+                    {...field}
+                    value={field.value ?? ""}
+                    type="password"
+                  />
+                </FormControl>
+                <FormMessage>{fieldState.error?.message}</FormMessage>
+              </FormItem>
+            )}
+          />
+          <Button variant="primary" className="w-full" type="submit">
+            Entrar
+          </Button>
+        </form>
+      </Form>
+    </main>
+  );
+}
+
+export function WelcomeBackScreen() {
+  const { welcomeBackAction } = useLoginRegisterFlow();
+
+  const goBack = useCallback(
+    () =>
+      welcomeBackAction({
+        command: "goBack",
+        nextStep: "greeting",
+      }),
+    [welcomeBackAction],
+  );
+
+  return (
+    <div className="flex min-h-screen flex-grow flex-col">
+      <header className="items-between flex justify-between pt-12">
+        <Button
+          className="ml-8 mt-8 gap-3 p-6 text-lg lg:ml-14"
+          variant="outline"
+          onClick={goBack}
+        >
+          <ArrowLeftIcon />
+          Voltar
+        </Button>
+        <div className="hidden px-12 md:block">
+          <NossaTerraLogo />
+        </div>
+      </header>
+
+      <WelcomeBackContent className="flex grow" />
+
+      <footer className="flex flex-col justify-center">
+        <ImageCarousel
+          pathArray={["", "", "", "", "", "", ""]}
+          height={220}
+          width={220}
+          className="hidden md:block"
+        />
+        <div className="flex justify-center p-12 md:hidden">
+          <NossaTerraLogo />
+        </div>
+      </footer>
+    </div>
+  );
+}
