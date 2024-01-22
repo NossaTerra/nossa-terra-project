df191eb2ef41e9176eaf6a563e655cddce1f65ce
Gustavo <pregustavomium@.@gmail.com>
2024-01-21 22:32:54 -0300

    feat: add reset password screen

---
+++ src/pages/reset-password/[token].tsx
@@ -0,0 +1,171 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { type ClassNameProps, cn } from "~/utils/ui";
+import { Button } from "~/components/ui/button";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { useCallback } from "react";
+import { ArrowLeftIcon } from "lucide-react";
+import { NossaTerraLogo } from "~/components/common/NossaTerraLogo";
+import { Input } from "~/components/ui/input";
+import { useRouter } from "next/router";
+import {
+  type ResetPasswordFields,
+  useResetPasswordSchema,
+} from "~/screens/LoginRegisterFlow/hooks/useResetPasswordSchema";
+import { api } from "~/utils/api";
+import { emptyString } from "~/utils/constants";
+import toast from "react-hot-toast";
+
+function ResetPasswordContent({
+  className,
+  token,
+}: ClassNameProps & { token?: string }) {
+  const schema = useResetPasswordSchema();
+  const router = useRouter();
+
+  const form = useForm<ResetPasswordFields>({
+    resolver: zodResolver(schema),
+  });
+
+  const validatePasswordResetToken =
+    api.forgetPassword.validatePasswordResetToken.useMutation();
+  const resetPassword = api.forgetPassword.resetPassword.useMutation();
+
+  const onSubmit: SubmitHandler<ResetPasswordFields> = useCallback(
+    async ({ password }) => {
+      if (token && token !== emptyString) {
+        try {
+          const userId = await validatePasswordResetToken.mutateAsync({
+            token,
+          });
+          await resetPassword.mutateAsync({ password, userId });
+          toast.success("Senha redefinida com sucesso!", {
+            duration: 1400,
+          });
+          await router.replace("/search");
+        } catch (err) {
+          toast.error("Erro ao redefinir a senha!");
+        }
+      }
+    },
+    [resetPassword, router, token, validatePasswordResetToken],
+  );
+
+  return (
+    <main
+      className={cn(
+        "flex flex-col items-start justify-start gap-8 md:justify-start",
+        "px-8 py-6 lg:px-14",
+        className,
+      )}
+    >
+      <h1
+        className={cn(
+          "font-poppins-800 text-headingPrimary",
+          "text-4xl lg:text-5xl",
+          "inline-block md:block",
+        )}
+      >
+        Redefinir a sua <span className="text-headingSecondary">Senha</span>
+      </h1>
+
+      <Form {...form}>
+        <form
+          className="grid w-full grid-cols-1 justify-start gap-x-16 gap-y-6"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field, fieldState }) => (
+              <FormItem className="mb-2.5 w-80 text-gray-700">
+                <FormLabel
+                  className="block text-sm font-medium"
+                  htmlFor="password"
+                >
+                  Nova Senha*
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="mt-3x w-80 md:mt-0"
+                    placeholder="Senha"
+                    {...field}
+                    type="password"
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage>{fieldState.error?.message}</FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field, fieldState }) => (
+              <FormItem className="mb-2.5 w-80 text-gray-700">
+                <FormLabel
+                  className="block text-sm font-medium"
+                  htmlFor="confirmPassword"
+                >
+                  Confirmar nova Senha*
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="mt-3x w-full md:mt-0"
+                    placeholder="Senha"
+                    {...field}
+                    type="password"
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+                <FormMessage>{fieldState.error?.message}</FormMessage>
+              </FormItem>
+            )}
+          />
+          <div>
+            <p className="pb-2.5 text-sm">*campo obrigatório</p>
+            <Button variant="primary" className="w-80" type="submit">
+              Redefinir Senha
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </main>
+  );
+}
+
+export default function ResetPasswordScreen() {
+  const router = useRouter();
+  const { query } = router;
+  const goBack = useCallback(() => {
+    router.back();
+  }, [router]);
+
+  return (
+    <div className="flex min-h-screen flex-grow flex-col">
+      <header className="items-between flex justify-between pt-12">
+        <Button
+          className="ml-8 mt-8 gap-3 p-6 text-lg lg:ml-14"
+          variant="outline"
+          onClick={goBack}
+        >
+          <ArrowLeftIcon />
+          Voltar
+        </Button>
+        <div className="hidden px-12 md:block">
+          <NossaTerraLogo />
+        </div>
+      </header>
+      <ResetPasswordContent
+        token={typeof query.token === "string" ? query.token : ""}
+      />
+    </div>
+  );
+}
