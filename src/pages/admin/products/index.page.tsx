ef439a4f40f0d50ad4baadcbd18203b888a1aad9
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-03-04 23:22:52 -0300

    feat(listings): add inactive user states

--- src/pages/admin/products/index.page.tsx
+++ src/pages/admin/products/index.page.tsx
@@ -27,6 +27,7 @@ import {
   ProductTypeLabel,
   getProductImageSrc,
 } from "~/server/types/product.type";
+import { ProductCard } from "~/components/common/ProductCard";
 
 export const getServerSideProps = redirectGetServerSideProps.Admin;
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
@@ -292,39 +293,6 @@ function ProductRow({ product }: { product: Product }) {
           </Dialog>
         </DialogContent>
       </Dialog>
-    </div>
-  );
-}
-
-function ProductCard({
-  product,
-  className,
-  ...rest
-}: { product: Product } & React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn(
-        "bg-background relative flex min-h-20 w-[23em] overflow-hidden rounded-lg border-[2.5px] border-gray-800 p-4",
-        className,
-      )}
-      {...rest}
-    >
-      <div
-        className="absolute -left-1 top-0 h-[110%] w-8"
-        style={{
-          backgroundColor: product.mainColor,
-        }}
-      />
-      <Image
-        priority
-        src={getProductImageSrc(product.type)}
-        height={90}
-        width={90}
-        alt="marca dágua do produto"
-        className="absolute -bottom-4 -right-3 opacity-60"
-      />
-
-      <div className="pl-6 pr-12 text-lg">{product.name}</div>
     </div>
   );
 }
--- src/pages/listings/index.page.tsx
+++ src/pages/listings/index.page.tsx
@@ -1,17 +1,107 @@
 import { type InferGetServerSidePropsType } from "next";
 import { AppHeader } from "~/components/common/headers";
+import Image from "next/image";
+import Link from "next/link";
+import { H1, P } from "~/components/ui/typography";
 import { redirectGetServerSideProps } from "~/server/api/auth/redirectGetServerSideProps";
+import { linkClassNames } from "~/components/ui/button";
+import { type ClassNameProps, cn } from "~/utils/ui";
 
 export const getServerSideProps = redirectGetServerSideProps.BuyerOnly;
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 export default function MyListingsScreen({ user }: Props) {
-  return (
-    <>
-      <AppHeader user={user} />
-      <div className="p-10">
-        <h1 className="text-4xl font-bold">Meus Anúncios</h1>
+  if (user.activeState !== "active") {
+    return (
+      <div className="flex h-fit min-h-dvh flex-1 flex-col">
+        <div className="z-10">
+          <AppHeader user={user} />
+          {user.activeState === "inactive" && (
+            <PendingActivationMessage className="px-16" />
+          )}
+          {user.activeState === "inactive_payment_problem" && (
+            <InactivePaymentMessage className="px-16" />
+          )}
+        </div>
+
+        <ImageBackgroundFooter src="/images/coffee-envelop-transparent.png" />
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div>
+      <AppHeader user={user} />
+      <div className="px-10">
+        <H1>Meus Anúncios</H1>
+      </div>
+    </div>
+  );
+}
+
+// NOTE: Possible optimization to reduce image sizes.
+// Serve a JPG with a circular gradient mask (which could be from a file or made programatically via "CSS").
+function ImageBackgroundFooter({ src }: { src: string }) {
+  return (
+    <div
+      className={cn(
+        "pointer-events-none z-0 -mt-[30vw] flex min-h-[70vh] flex-1 flex-col items-end justify-end overflow-hidden",
+      )}
+    >
+      <div className="relative -mb-32 flex h-full max-h-[70vh] w-full flex-1">
+        <Image
+          src={src}
+          objectFit="contain"
+          objectPosition="right 0% bottom 50%"
+          alt="Imagem de fundo"
+          fill
+        />
+      </div>
+    </div>
+  );
+}
+
+function PendingActivationMessage({ className }: ClassNameProps) {
+  return (
+    <div className={cn("w-fit", className)}>
+      <H1>Aguardando Aprovação</H1>
+      <div className="max-w-3xl">
+        <P className="md:text-lg lg:text-xl">
+          Para que possa fazer seus anúncios, seu cadastro está em análise pelo
+          nosso time. Esse processo pode levar até{" "}
+          <span className="font-bold">2 dias</span> úteis.
+        </P>
+        <P className="md:text-lg lg:text-xl">
+          Se tiver alguma dúvida ou precisar de ajuda durante esse período,
+          fique à vontade para
+          <Link href="/contact" className={cn(linkClassNames, "px-1")}>
+            entrar em contato
+          </Link>
+        </P>
+        <P className="md:text-lg lg:text-xl">Estamos aqui para ajudar!</P>
+      </div>
+    </div>
+  );
+}
+
+function InactivePaymentMessage({ className }: ClassNameProps) {
+  return (
+    <div className={cn("w-fit", className)}>
+      <H1>Atualização de Pagamento Necessária</H1>
+      <div className="max-w-3xl">
+        <P className="md:text-lg lg:text-xl">
+          Seu último pagamento não foi processado e seus anúncios foram pausados
+          temporariamente. Atualize suas informações de pagamento para retomar
+          os seus anúncios.
+        </P>
+        <P className="md:text-lg lg:text-xl">
+          Para ajuda ou mais informações{" "}
+          <Link href="/contact" className={cn(linkClassNames, "px-1")}>
+            entre em contato
+          </Link>
+        </P>
+        <P className="md:text-lg lg:text-xl">Estamos aqui para ajudar!</P>
+      </div>
+    </div>
   );
 }
