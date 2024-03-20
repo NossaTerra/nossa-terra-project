8798f958e6986aa02e62f62ee389e5b0e5f3f568
Gustavo <pregustavomium@.@gmail.com>
2024-03-19 22:27:39 -0300

    feat: update productCard styles

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { cn } from "~/utils/ui";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  price?: string;
+  small?: boolean;
   product: Product;
   footer?: React.ReactNode;
   topRightElement?: React.ReactNode;
@@ -13,13 +15,15 @@ export function ProductCard({
   product,
   footer,
   topRightElement,
+  small,
   className,
   ...rest
 }: Props) {
   return (
     <div
       className={cn(
-        "bg-background relative flex min-h-20 w-[23em] overflow-hidden rounded-lg border-[2.5px] border-gray-800 p-4",
+        "bg-background relative flex min-h-20 overflow-hidden rounded-lg border-[2.5px] border-gray-800 p-4",
+        small ? "w-[19em]  xl:w-[22em]" : "w-[23em]",
         className,
       )}
       {...rest}
@@ -33,18 +37,18 @@ export function ProductCard({
       <Image
         priority
         src={getProductImageSrc(product.type)}
-        height={90}
-        width={90}
+        height={small ? 62 : 90}
+        width={small ? 62 : 90}
         alt="marca dágua do produto"
         className="absolute -bottom-4 -right-3 opacity-60"
       />
-      <div className="pl-6 pr-16">
+      <div className={small ? "mb-2 pl-5 pr-2" : "pl-6 pr-16"}>
         <p className="text-lg">{product.name}</p>
         {footer}
       </div>
 
       {topRightElement && (
-        <div className="right-2 top-2">{topRightElement}</div>
+        <div className={cn("right-2 top-2")}>{topRightElement}</div>
       )}
     </div>
   );
