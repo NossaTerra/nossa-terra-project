0a00da55735fbb291e52afbd744e152eaf691a35
Gustavo <pregustavomium@.@gmail.com>
2024-03-29 13:49:05 -0300

    fix: fix a bug where seller coords were not updating

--- src/pages/login/LoginRegisterFlow/screens/SecondDataStepSellerScreen.tsx
+++ src/pages/login/LoginRegisterFlow/screens/SecondDataStepSellerScreen.tsx
@@ -438,6 +438,8 @@ export function SellerForm({
             isEditing={isEditingProfile}
             user={user}
             form={form}
+            userLatitude={latitude}
+            userLongitude={longitude}
           />
         </form>
       </Form>
