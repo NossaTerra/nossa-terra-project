84357d6dc1b5c34499217355d002756aab96e6a8
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-04-05 17:31:00 -0300

    style(input|select): strengthen border and add better disabled state

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -74,9 +74,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <div className="flex items-center gap-3" ref={parentDivRef}>
         <input
           className={cn(
-            "flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
-            // Custom Theme overrides
-            "bg-backgroundPrimary",
+            "flex h-10 w-full rounded-md border border-slate-400 bg-backgroundPrimary px-3 py-2 text-sm text-black ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-200 disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
             className,
           )}
           onInput={onInput}
